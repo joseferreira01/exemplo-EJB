67e7d18b39f7889ca3a77d1402c3a7291d259178
joseferreira01 <joseifpb2015@gmail.com>
2017-12-13 00:45:13 -0200

    rodando com docker-compose

--- kill.sh
+++
@@ -1,9 +0,0 @@
-docker-compose down
-docker rm appcore -f
-docker rmi ejb-core
-
-# parar app jsf
-
-#docker stop appjsf
-docker rm appjsf -f
-docker rmi ejb-jsf
--- run.sh
+++
@@ -1,3 +0,0 @@
-mvn clean install
-docker-compose up -d
-#docker logs -f $(docker ps -q -f name=" appcore")
