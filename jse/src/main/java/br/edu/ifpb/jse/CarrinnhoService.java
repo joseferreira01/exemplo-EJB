74855cf18da186414fcba51da4e7dd7935ea8f26
joseferreira01 <joseifpb2015@gmail.com>
2017-12-18 15:44:16 -0200

    add jse ok

---
+++ jse/src/main/java/br/edu/ifpb/jse/CarrinnhoService.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.jse;
+
+import br.edu.ifpb.shared.Carrinho;
+import br.edu.ifpb.shared.ServiceLocator;
+import java.util.List;
+
+/**
+ *
+ * @author jose
+ */
+public class CarrinnhoService implements Carrinho {
+
+    private Carrinho carrinho;
+    private ServiceLocator locator;
+
+    public CarrinnhoService() {
+        this.locator = new ServiceLocator();
+        carrinho = locator
+                .lookup("java:global/core/CarrinhoOnline", Carrinho.class);
+        carrinho.add("tv");
+        carrinho.add("cama");
+        carrinho.add("mesa");
+    }
+
+    @Override
+    public void add(String produto) {
+        carrinho.add(produto);
+    }
+
+    @Override
+    public void remover(String produto) {
+        carrinho.remover(produto);
+    }
+
+    @Override
+    public void finalizar() {
+        carrinho.finalizar();
+    }
+
+    @Override
+    public List<String> todosProdutos() {
+        return carrinho.todosProdutos();
+    }
+
+}
